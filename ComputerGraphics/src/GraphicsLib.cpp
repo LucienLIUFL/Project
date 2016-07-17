e455ddd43be64c17f7f75f961213da9aab7c23bf
LucienLIUFL <maples0108@gmail.com>
2016-07-17 21:33:22 +0800

    Modify

--- ComputerGraphics/src/GraphicsLib.cpp
+++ ComputerGraphics/src/GraphicsLib.cpp
@@ -20,18 +20,14 @@ void GraphicsLib::drawStar() {
 
 void GraphicsLib::drawCycle(int n, double extend) {
     double angle = (3.1415926 * 2 / n);
-    double x = 50, y = 50;
-
+    double x = 0, y = 50;
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0, 0.4, 0.2);
-    glBegin(GL_LINE_STRIP);
+    glColor3f(0.0, 1, 0.2);
+    glBegin(GL_POLYGON);
     glVertex2f(x, y);
     for (int i = 0; i < n; ++i) {
-        std::cout << (i * angle) << std::endl;
         x += (extend * sin(i * angle));
         y += (extend * cos(i * angle));
-        std::cout << sin(i * angle) << " " << std::ends;
-        std::cout << cos(i * angle) << std::endl;
         glVertex2f(x, y);
     }
     glEnd();
