154abdaa2ae130f35e37fc24cd7db9cc526e2e2d
LucienLIUFL <maples0108@gmail.com>
2016-07-25 22:23:35 +0800

    BMPLIB acomplish

--- ComputerGraphics/src/BMPLib.cpp
+++ ComputerGraphics/src/BMPLib.cpp
@@ -1,1 +1,20 @@
 #include "BMPLib.h"
+#include <cstdio>
+#include <string>
+#include <memory>
+
+std::shared_ptr<BMPLib::BmpInfo> BMPLib::makeBmpInfo(const std::string & filename) {
+    FILE * file = fopen(filename.c_str(), "r");
+    std::shared_ptr<BMPLib::BmpInfo> bmpInfo = std::make_shared<BMPLib::BmpInfo>();
+
+    BMPLib::Bmp bmp;
+    fseek(file, 0, 0);
+    fread(&bmp, sizeof(BMPLib::Bmp), 1, file);
+    bmpInfo->imageData = new unsigned char[bmp.biSizeImage];
+    fseek(file, (long)bmp.bfOffBits, 0);
+    fread(bmpInfo->imageData, sizeof(unsigned char), bmp.biSizeImage, file);
+    fclose(file);
+
+    bmpInfo->bmp = bmp;
+    return bmpInfo;
+}
--- ComputerGraphics/src/BMPLib.h
+++ ComputerGraphics/src/BMPLib.h
@@ -1,11 +1,46 @@
 #ifndef BMPLIB_H_
 #define BMPLIB_H_
+#include <string>
+#include <memory>
 
 
-struct BMP {
-    unsigned short bfType;
-    unsigned int bfSize;
+namespace BMPLib {
+    struct Bmp {
+        // 14 Bytes (BMP File Header)
+        unsigned short bfType; // Label of BMP file
+        unsigned int bfSize;
+        unsigned short bfReservedFirst;
+        unsigned short bfReservedSecond;
+        unsigned int bfOffBits;
 
-};
+        // 40 Bytes (BMP File Information Header)
+        unsigned int  biSize;
+        int    biWidth;
+        int    biHeight;
+        unsigned short   biPlanes;            // Always 1
+        unsigned short   biBitCount;        //Bits of BMP File (1 4 8 16 24 32)
+        unsigned int  biCompression;
+        unsigned int  biSizeImage;
+        int   biXPelsPerMeter;
+        int   biYPelsPerMeter;
+        unsigned int  biClrUsed;
+        unsigned int  biClrImportant;
+    };
+
+    class BmpInfo {
+    public:
+        friend std::shared_ptr<BmpInfo> makeBmpInfo(const std::string & filename);
+
+        BmpInfo() : imageData(nullptr) {}
+        virtual ~BmpInfo() {
+            delete imageData;
+        }
+    private:
+        Bmp bmp;
+        unsigned char * imageData;
+    };
+
+    std::shared_ptr<BmpInfo> makeBmpInfo(const std::string & filename);
+}
 
 #endif
