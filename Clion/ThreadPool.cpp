806d2842b8f8431a00c0710f25109b973a6bbc16
LucienLIUFL <maples0108@gmail.com>
2016-09-03 17:18:31 +0800

    present

--- Clion/ThreadPool.cpp
+++ Clion/ThreadPool.cpp
@@ -4,8 +4,19 @@
 
 #include "ThreadPool.h"
 
-Maples::ThreadPool::ThreadPool(size_t threadNumber) {
+Maples::ThreadPool::ThreadPool(size_t threadNumber) : stop(false) {
+    for (size_t i = 0; i < threadNumber; ++i) {
+        this->workers.emplace_back(
+            [this] {
+                for(;;) {
+                    std::function<void()> task;
+                    {
 
+                    }
+                }
+            }
+        );
+    }
 }
 
 Maples::ThreadPool::~ThreadPool() {
@@ -15,7 +26,25 @@ Maples::ThreadPool::~ThreadPool() {
 template<typename F, typename... Args>
 auto Maples::ThreadPool::enqueue(F && func, Args && ... args)
     ->std::future<typename std::result_of<F(Args...)>::type> {
-    return std::future << unknown >> ();
+
+    using ReturnType = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<ReturnType()>>(
+        std::bind(std::forward<F>(func), std::forward<Args>(args)...)
+    );
+
+    std::future<ReturnType> res = task->get_future();
+
+    {
+        std::unique_lock<std::mutex> lock(this->queueMutex);
+        if (this->stop) {
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+        }
+        this->tasks.emplace([task]{(*task)();});
+    }
+
+    condition.notify_one();
+    return res;
 }
 
 
