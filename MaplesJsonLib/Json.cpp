ae0dd7091785a628b600150c584e826f9dd34870
LucienLIUFL <maples0108@gmail.com>
2017-01-19 09:29:13 +0800

    Json lib

--- MaplesJsonLib/Json.cpp
+++ MaplesJsonLib/Json.cpp
@@ -7,8 +7,22 @@
 using namespace Maples;
 
 bool Value::parse(const std::string & context) {
-    
-    return false;
+    if (context.empty()) {
+        return false;
+    }
+
+//    std::string::iterator iter = context.begin();
+    switch (context.at(0)) {
+        case 'n' :
+            return this->parseNull(context);
+        case 'f' :
+            return this->parseFalse(context);
+        case 't' :
+            return this->parseTrue(context);
+
+        default:
+            return false;
+    }
 }
 
 bool Value::parseNull(const std::string & context) {
