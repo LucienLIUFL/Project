0423a8fce20118caf827178a83e490cde6170c51
LucienLIUFL <maples0108@gmail.com>
2017-02-05 08:44:01 +0800

    m

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <future>
+
+
+int add(int a, int b) {
+    std::cout << "Hello, World!" << std::endl;
+    return a + b;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::future<int> result1 = std::async(add, 1, 2);
+    std::future<int> result2 = std::async(add, 3, 4);
+//    std::cout << "Hello, World!" << std::endl;
+    std::cout << result1.get() << std::endl;
+    std::cout << result2.get() << std::endl;
     return 0;
 }
