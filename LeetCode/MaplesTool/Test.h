96016a55eddba91f6d13926d5dd4a6c8ae61738c
LucienLIUFL <maples0108@gmail.com>
2017-03-02 21:05:35 +0800

    m

--- LeetCode/MaplesTool/Test.h
+++ LeetCode/MaplesTool/Test.h
@@ -5,34 +5,7 @@
 #ifndef LEETCODE_TEST_H
 #define LEETCODE_TEST_H
 
-class Object {
-public:
-    virtual void print();
-};
 
-class Father : public Object {
-public:
-    virtual void print() override;
-};
-
-class Mother : public Object {
-public:
-    virtual void print() override;
-};
-
-class Child : public Father, public Mother {
-public:
-    virtual void print() override;
-};
-
-class Test {
-public:
-    static void test0();
-    static void test1();
-    static void test2();
-    static void test3();
-    static void test4();
-};
 
 
 #endif //LEETCODE_TEST_H
--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 
 #include "MaplesTool/Test.h"
 
 int main(int argc, const char * argv[]) {
-    Test::test1();
+
     return 0;
 }
