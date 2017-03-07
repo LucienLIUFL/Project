5a0d23abae6548faf3edbab1c95d7e1ef0cbb225
LucienLIUFL <maples0108@gmail.com>
2017-03-07 21:14:08 +0800

    m

--- LeetCode/MaplesTool/Maples.cpp
+++ LeetCode/MaplesTool/Maples.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Maples.h"
+
+void adjustDown(int * const heap, int root, int len) {
+    int rootValue = heap[root];
+    for (int i = 2 * root + 1; i < len; ++i) {
+        i = ((i < len - 1) && (heap[i] < heap[i + 1])) ? i + 1 : i;
+        if (rootValue < heap[i]) {
+            rootValue = heap[i];
+            root = i;
+        }
+    }
+    heap[root] = rootValue;
+}
--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,7 +1,24 @@
-
+#include <iostream>
 #include "MaplesTool/Test.h"
 
-int main(int argc, const char * argv[]) {
 
+
+
+void test() {
+    for (int i = 0; i < 154; ++i) {
+        for (int j = 0; j < 154; ++j) {
+            for (int k = 0; k < 154; ++k) {
+                if ((8 * i + 10 * j + 20 * k) == 154) {
+                    std::cout << i << " " << j << " " << k << std::endl;
+                }
+            }
+        }
+    }
+
+}
+
+
+int main(int argc, const char * argv[]) {
+    test();
     return 0;
 }
