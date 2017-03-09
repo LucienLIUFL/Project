40318f0a51a5e368badd6c845088c28569e6af9d
LucienLIUFL <maples0108@gmail.com>
2017-03-09 17:35:55 +0800

    m

--- Master/src/Main.cpp
+++ Master/src/Main.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <iterator>
-#include <algorithm>
-
-#include "Algorithm.hpp"
-
-
-void show(const std::vector<int> & array) {
-	std::for_each(std::begin(array), std::end(array), [](int x){
-		std::cout << x << "\t" << std::ends;
-	});
-	std::cout << std::endl;
-}
 
 int main(int argc, const char * argv[]) {
-    // std::vector<int> array0 = {1, 3, 5, 6, 8, 9}; // Size = 6;
-    // std::vector<int> array1 = {2, 9, 4, 2, 1, 3, 1, 5, 6, 8, 0}; // Size = 11;
-    // std::cout << Algorithm::binarySearch(array,11, 8) << std::endl;
-    // Algorithm::selectSort(&array0.at(0), array0.size());
-    // Algorithm::selectSort(&array1.at(0), array1.size());
-	// show(array0);
-	// show(array1);
-	// std::vector<int> array2(17);
-	// Algorithm::mergeSort(&array0.at(0), &array1.at(0), array0.size(), array1.size(), &array2.at(0));
-	// show(array2);
-    std::vector<int> heap = {53, 17, 78, 9, 45, 65, 87, 32};
-	Algorithm::heapSort(&heap[0], heap.size());
-	Algorithm::bubbleSort(&heap[0], heap.size());
-	show(heap);
+	int p, e, i, d;
+	int count = 1;
+	while (std::cin >> p >> e >> i >> d) {
+		if (p == -1 && e == -1 && e == -1 && d == -1) {
+			break;
+		}
+		p %= 23;  
+        e %= 28;  
+        i %= 33; 
+
+		for (int date = d + 1;; date++) {
+			if (((date - p) % 23 == 0) && ((date - e) % 28 == 0) && ((date - i) % 33 == 0)) {
+				std::cout << "Case " << count++ <<
+						": the next triple peak occurs in " << (date - d) << std::endl;
+				break;
+			}
+		}		
+	}	
+	return 0;
 }
